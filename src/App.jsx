023c3693a2ce37e84d173327302e1356534a623a
Jakhongir <jakhongiryusupov58@gmail.com>
2022-12-05 18:32:38 +0500

    header-top finished

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+export default function index() {
+  return (
+    <>
+      <Route exact path="/"></Route>
+    </>
+  )
+}
